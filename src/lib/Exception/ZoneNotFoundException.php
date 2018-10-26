f4b57d3cd7dc3b00e4c6977b18542208370b8f19
Mateusz Bieniek <mateusz.bieniek@ez.no>
2018-10-26 14:21:04 +0200

    Fixes after CR

--- src/lib/Exception/ZoneNotFoundException.php
+++ src/lib/Exception/ZoneNotFoundException.php
@@ -9,21 +9,16 @@ namespace EzSystems\EzPlatformPageMigration\Exception;
 use EzSystems\EzPlatformPageFieldType\FieldType\LandingPage\Definition\LayoutDefinition;
 use RuntimeException;
 
-/**
- * No zone found.
- */
 class ZoneNotFoundException extends RuntimeException
 {
     /**
-     * ZoneNotFoundException constructor.
-     *
      * @param string $zoneId
      * @param \EzSystems\EzPlatformPageFieldType\FieldType\LandingPage\Definition\LayoutDefinition $layout
      */
     public function __construct(string $zoneId, LayoutDefinition $layout)
     {
         parent::__construct(
-            'Zone with id: "' . $zoneId . '"" not found for layout: "' . $layout->getName() . '"", id: "' . $layout->getId() . '"'
+            sprintf('Zone with id: "%s" not found for layout: "%s", id: "%s"', $zoneId, $layout->getName(), $layout->getId())
         );
     }
 }
